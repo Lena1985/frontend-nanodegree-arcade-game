496eff7e92dd913aedeb13900cfe638e6f6ca7b1
Lena1985 <lena.bieger@icloud.com>
2017-04-30 10:47:55 +0200

    Add project files to repo

---
+++ README.md
@@ -0,0 +1,29 @@
+frontend-nanodegree-arcade-game
+===============================
+
+Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+
+For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+
+
+## Getting started
+---------------
+
+This is a version of the Arcade Game "Frogger".
+There is no need to install anything before you can start playing the game. The game runs on any browser which has JavaScript enabled.
+Simply open the index.html file in the browser.
+
+## How to play
+---------------
+
+You have a **Player** and **Enemies** in this Game.
+The goal of the game is that the player has to _reach the water._
+The bugs which are running at different speeds from left to right are your enemies.
+You have to cross the grass, the stones and get to the water without colliding with them. Beware of the enemies, once you collide with them, the game resets and you have to start again.
+If you get to the water succesfully, the game also resets and you can play it again.
+
+#### The following keys can be used to control the player:
+* up arrow: to move the player up
+* down arrow: to move the player down
+* left arrow: to move the player left
+* right arrow: to move the player right
---
+++ js/app.js
@@ -0,0 +1,102 @@
+// Create the enemy class
+var Enemy = function(x, y) {
+  // Select the enemy bug image
+	this.sprite = 'images/enemy-bug.png';
+	
+	// Define position(x and y coordinates on the canvas) and speed of the Enemies
+	this.x = x;
+	this.y = y;
+	this.speed = Math.floor((Math.random()*230)+100);
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+	// Movement is multiplied by the dt parameter,
+	// which ensures the game runs at the same speed for
+	// all computers.
+	// Set the speed if the enemy is visible on the screen	
+	if(this.x <= 500) {
+		this.x += this.speed * dt;
+		// Reset the enemy position if it has crossed the screen
+	} else {
+		this.x = -10;
+	}
+	
+	// Reset the game when the player comes within 25px of an enemy bug
+	if(player.x <= this.x + 25 && player.x >= this.x - 25) {
+		if(player.y <= this.y + 25 && player.y >= this.y - 25) {
+			player.reset();
+		}
+	}
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Create the player class and set the player's start position
+var Player = function() {
+	this.sprite = 'images/char-cat-girl.png';
+	this.x = 200;
+	this.y = 400;
+};
+
+// Update the player position when any of the movement keys are pressed
+// Avoid that the player can move off the screen
+Player.prototype.update = function() {
+	if(this.ctlKey === 'left' && this.x > 0) {
+		this.x = this.x - 101;
+	} else if(this.ctlKey === 'right' && this.x != 402) {
+		this.x = this.x + 101;
+	} else if(this.ctlKey === 'up') {
+		this.y = this.y - 85;
+	} else if(this.ctlKey === 'down' && this.y != 365) {
+		this.y = this.y + 85;
+	}
+	this.ctlKey = null;
+	// Reset the player position if player reaches water
+	if(this.y < 25) {
+		this.reset();
+	}
+};
+
+// Define the inital position of the player on the canvas and on reset
+Player.prototype.reset = function() {
+	player.x = 200;
+	player.y = 400;
+}
+// Draw the Player on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(e){
+	this.ctlKey = e;
+}
+
+// Create array of enemies with inital position
+var allEnemies = [];
+(function generateEnemies() {
+	allEnemies.push(new Enemy(-10, 60));
+	allEnemies.push(new Enemy(-10, 145));
+	allEnemies.push(new Enemy(-10, 230));
+	allEnemies.push(new Enemy(-10, 315));
+}());
+
+// Create the player, an instance of the Player class previously defined
+var player = new Player();
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
